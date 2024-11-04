6238ca8a34222924aa5961e6e690f32566daadf1
baroiancristina <Baroian.Ma.Cristina@student.utcluj.ro>
2024-11-04 05:03:33 +0200

    tema 2

--- tema2.py
+++ tema2.py
@@ -1,13 +1,12 @@
-str="=I want you back!"
-jumatate= (str//2)
-prima_parte = [ ::jumatate]
-a_doua_parte= [jumatate:: ]
-prima_parte = prima_parte.uppercase()
-prima parte = prima_parte.strip()
+string="I want you back!."
+jumatate = len(string)//2
+prima_parte = string[ :jumatate]
+a_doua_parte = string[jumatate: ]
+prima_parte = prima_parte.upper()
+prima_parte = prima_parte.strip()
 a_doua_parte = a_doua_parte[::-1]
-a_doua_parte = a_doua_parte.capitalize
+a_doua_parte = a_doua_parte.capitalize()
 a_doua_parte = a_doua_parte.translate(str.maketrans('', '', string.punctuation))
 rezultat = prima_parte + " " + a_doua_parte
-return rezultat
 print(rezultat)
 
